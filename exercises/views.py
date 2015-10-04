598052e8bd577810155049e858528efabe5ed8ff
Aleksis Brezas <abresas@gmail.com>
2015-10-04 13:58:20 +0100

    add user to exercises template context

--- exercises/views.py
+++ exercises/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'exercises/index.html')
+    context = {'user': request.user}
+    return render(request, 'exercises/index.html', context)
