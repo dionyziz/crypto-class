f928bdd598a1c497772974e4846ceec92c229107
Aleksis Brezas <abresas@gmail.com>
2015-10-04 13:57:24 +0100

    improve login page

--- cryptoclass/forms.py
+++ cryptoclass/forms.py
@@ -5,3 +5,7 @@ from django.contrib.auth.forms import AuthenticationForm
 class CryptoAuthenticationForm(AuthenticationForm):
     username = forms.CharField(label=u"Όνομα χρήστη", max_length=75)
     password = forms.CharField(label=u"Κωδικός", widget=forms.PasswordInput)
+    error_messages = {
+        'invalid_login': "Ο κωδικός ή το όνομα χρήστη δεν είναι σωστός. Προσπάθησε ξανά.",
+        'inactive': "Αυτός ο λογαριασμός είναι απενεργοποιημένος."
+    }
