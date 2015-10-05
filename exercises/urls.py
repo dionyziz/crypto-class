5b0e4512220d3a2ddfc2af1030d395a3584eca17
Konstantinos Kanellis <kkanelli@hotmail.com>
2015-10-05 18:46:24 +0300

    add basic exercises routing & views

--- exercises/urls.py
+++ exercises/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^exercises/$', views.index, name='index'),
+    url(r'^exercises/(?P<exercise_tag>[0-9.]+)/$', views.detail, name='detail'),
+    #url(r'^exercises/(?P<exercise_tag>[0-9.]+)/submit', views.submit, name='submit'),
     url(r'^$', views.homepage, name='homepage'),
 ]
--- exercises/views.py
+++ exercises/views.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import SubmittableExercise
 
 def homepage(request):
     context = {'user': request.user}
     return render(request, 'index.html', context)
 
 def index(request):
-    context = {'user': request.user}
+    exercise_list = SubmittableExercise.objects.order_by('tag')
+
+    context = {
+            'user': request.user,
+            'exercise_list': exercise_list
+        }
     return render(request, 'exercises/index.html', context)
+
+def detail(request, exercise_tag):
+    exercise = get_object_or_404(SubmittableExercise, tag=exercise_tag)
+
+    #TODO: Check if user sees the exercise for the first time
+    #       If so, generate the appropriate data
+
+    context = {
+            'user': request.user,
+            'exercise': exercise
+        }
+    return render(request, 'exercises/detail.html', context)
