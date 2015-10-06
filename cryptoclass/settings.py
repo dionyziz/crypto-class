997680274c3329c5ae5137efad42b6ba84234c9f
Aleksis Brezas <abresas@gmail.com>
2015-10-06 13:27:58 +0100

    get secret key and debug settings from environment

--- cryptoclass/settings.py
+++ cryptoclass/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'htc4ss^d_)-dj)6n=eqbbocfm)rsg2p53qpz4c+wzi(&xj*cew'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'htc4ss^d_)-dj)6n=eqbbocfm)rsg2p53qpz4c+wzi(&xj*cew')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
