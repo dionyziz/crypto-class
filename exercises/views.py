0de34a997ee31507dde2d6e6f3b5702024a96659
Konstantinos Kanellis <kkanelli@hotmail.com>
2015-10-06 21:31:31 +0300

    implement submit functionallity in auto-grading exercises

--- exercises/views.py
+++ exercises/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from .models import BonusLink, BonusView, SubmittableExercise
+from django.core.urlresolvers import reverse
+from django.utils import timezone
 
+from .models import BonusLink, BonusView, SubmittableExercise, Submission
+from .forms import DocumentForm, TextAnswerForm
 
 def homepage(request):
     context = {'user': request.user}
@@ -12,21 +15,57 @@ def homepage(request):
 def index(request):
     exercise_list = SubmittableExercise.objects.order_by('tag')
 
+    active_exercises = [ exercise for exercise in exercise_list if exercise.deadline >= timezone.now() ]
+    past_exercises = [ exercise for exercise in exercise_list if exercise not in active_exercises ]
+
     context = {
             'user': request.user,
-            'exercise_list': exercise_list
+            'active_exercises': active_exercises,
+            'past_exercises': past_exercises,
         }
     return render(request, 'exercises/index.html', context)
 
 def detail(request, exercise_tag):
     exercise = get_object_or_404(SubmittableExercise, tag=exercise_tag)
 
+    user = request.user
+    submissions = exercise.submissions.filter(user=user).order_by('-time_submitted')
+
+    if exercise.type == exercise.AUTO_GRADING:
+        if request.method == 'POST':
+            form = TextAnswerForm(request.POST)
+
+            if form.is_valid():
+                answer = form.cleaned_data['answer']
+
+                # Call grader here
+                is_solution = True
+
+                # Save submission
+                submission = Submission.objects.create(
+                        user=user,
+                        time_submitted=timezone.now(),
+                        answer=answer,
+                        is_solution=is_solution
+                        )
+                exercise.submissions.add(submission)
+
+                return HttpResponseRedirect(reverse('exercise_detail', kwargs={'exercise_tag': exercise_tag} ))
+        else:
+            form = TextAnswerForm()
+
+    elif exercise.type == exercise.THEORETICAL:
+        pass
+
     #TODO: Check if user sees the exercise for the first time
     #       If so, generate the appropriate data
 
     context = {
-            'user': request.user,
-            'exercise': exercise
+            'user': user,
+            'exercise': exercise,
+            'submissions': submissions,
+            'form': form,
+            'can_be_submitted': exercise.deadline >= timezone.now(),
         }
     return render(request, 'exercises/detail.html', context)
 
