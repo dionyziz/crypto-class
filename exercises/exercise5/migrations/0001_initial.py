cf301c8bee3ab57916b6df1c901a80ccb891a3dd
Konstantinos Kanellis <kkanelli@hotmail.com>
2016-01-14 20:01:54 +0200

    add migrations for exercise5

---
+++ exercises/exercise5/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='HttpsPassword',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('password', models.TextField()),
+                ('filename', models.TextField()),
+            ],
+        ),
+    ]
